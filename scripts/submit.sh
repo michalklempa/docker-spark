dffbb7e7e436e57fc8e3a5e59be4f912b6cbff5f
Michal Klempa <michal.klempa@relayr.io>
2020-10-13 09:25:31 +0200

    .

--- scripts/submit.sh
+++ scripts/submit.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 env
 
+export SPARK_PUBLIC_DNS=$(hostname -i)
+
 java ${JAVA_OPTS} \
   -cp "${PROJECT_HOME}/jars/*" \
   org.apache.spark.deploy.SparkSubmit \
   --deploy-mode client \
   --master spark://${SPARK_MASTER_HOST}:${SPARK_MASTER_PORT} \
   --class ${MAIN_CLASS} \
+  --conf 'spark.driver.host='$(hostname -i) \
   local:///${PROJECT_BASE_DIR}/${JAR}
--- scripts/worker.sh
+++ scripts/worker.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 env
 
+export SPARK_PUBLIC_DNS=$(hostname -i)
+
 java ${JAVA_OPTS} \
     -cp "${PROJECT_HOME}/jars/*" \
     org.apache.spark.deploy.worker.Worker \
